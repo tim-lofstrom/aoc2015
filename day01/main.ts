0310901c775e4e1959123b4159e6c41a8e31ccbe
Löfström Danielsson Tim <tim.lofstrom.danielsson@lm.se>
2024-10-25 15:09:11 +0200

    update

--- day01/main.ts
+++ day01/main.ts
@@ -1,21 +1,27 @@
-import { map, reduce, split } from "lodash";
+import { first, last, map, reduce, rest, split, tail } from "lodash";
 import { readInputLine } from "../utils/file-utils";
 
 const input = readInputLine(__dirname, "input.txt");
-
 const parts = split(input, "");
 
-const result = reduce(
-  parts,
-  (acc, curr) => {
-    if (curr === "(") {
-      return acc + 1;
-    } else if (curr === ")") {
-      return acc - 1;
-    }
-    return acc;
-  },
-  0
-);
+function calcNext(acc: number, curr: string | undefined) {
+  if (curr === "(") {
+    return acc + 1;
+  } else if (curr === ")") {
+    return acc - 1;
+  }
+  return acc;
+}
 
-console.log(result);
+function basement(parts: string[] | undefined, floor: number): number {
+  if (floor === -1) return 0;
+  const f = first(parts);
+  const t = tail(parts);
+  return basement(t, calcNext(floor, f)) + 1;
+}
+
+const part1 = reduce(parts, (acc, curr) => calcNext(acc, curr), 0);
+const part2 = basement(parts, 0);
+
+console.log(part1);
+console.log(part2);
