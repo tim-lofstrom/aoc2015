a89bd552b9824b88bb381d2abdaa642f0625e637
Löfström Danielsson Tim <tim.lofstrom.danielsson@lm.se>
2024-10-30 07:06:41 +0100

    update

--- day02-fs/Program.fs
+++ day02-fs/Program.fs
@@ -11,6 +11,8 @@ let example = readFileAsArray "day02-fs/example.txt"
 let area length width height =
     [| (length * width); (width * height); (height * length) |]
 
+let arrayProduct (arr: int[]) = arr |> Array.fold (*) 1
+
 let calcDimensions (arr: int array) =
     match arr with
     | [| length; width; height |] -> area length width height
@@ -22,6 +24,12 @@ let calculateBox (dimensions: int array) =
     let dimensions = sortedDimensions |> Array.sum
     smallest + dimensions * 2
 
+let calculateRibbon (dimensions: int array) =
+    let sortedDimensions = dimensions |> Array.sort
+    let perimeter = 2 * (sortedDimensions[0..1] |> Array.sum)
+    let bow = dimensions |> arrayProduct
+    bow + perimeter
+
 let toIntArray = (fun (line: string) -> line.Split('x') |> Array.map int)
 
 let part1 =
@@ -29,4 +37,10 @@ let part1 =
     |> Array.map toIntArray
     |> Array.fold (fun sum dimensions -> sum + (calculateBox dimensions)) 0
 
+let part2 =
+    input
+    |> Array.map toIntArray
+    |> Array.fold (fun sum dimensions -> sum + (calculateRibbon dimensions)) 0
+
 printfn "%i" part1
+printfn "%i" part2
